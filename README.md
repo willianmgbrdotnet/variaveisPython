b3a3baf69dd9be706fa469029680bb97b2f5ed1c
WillianMGBrdotnet <willianmgbr@hotmail.com>
2024-06-19 10:31:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Declarando variáveis no Python e conferindo seus tipos com a função 'type()'. 
+
+###No Python, os tipos de variáveis são denominados 'class'. 
+##O que nos remete à Orientação a Objetos, onde o tipo do objeto é a classe da qual ele deriva.
---
+++ variaveis.py
@@ -0,0 +1,15 @@
+# Apenas declarando o valor, o Python já identifica o 'tipo' da variável.
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'willianDotnet', 40
+status = True
+
+# função 'type'
+print(type(media))
+print(type(n2))
+print(type(n4))
+print(type(nome))
+print(type(idade))
+print(type(status))
+
+# atalho 'ALT + SHIFT + SETA PRA BAIXO' copia a linha atual para a linha de baixo.
