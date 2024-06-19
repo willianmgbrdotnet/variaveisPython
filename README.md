ff681c47a7070d2a798c2b4e87140a57786ed519
WillianMGBrdotnet <willianmgbr@hotmail.com>
2024-06-19 11:13:26 -0300

    Verificando o tipo com a função isinstance()

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Declarando variáveis no Python e conferindo seus tipos com a função 'type()'. 
 
-###No Python, os tipos de variáveis são denominados 'class'. 
-##O que nos remete à Orientação a Objetos, onde o tipo do objeto é a classe da qual ele deriva.
+### No Python, os tipos de variáveis são denominados 'class'. 
+## O que nos remete à Orientação a Objetos, onde o tipo do objeto é a classe da qual ele deriva. 
+
+### Podemos também usar a função 'isinstance()', declarando a variável e logo em seguida, informando o tipo. "Perguntando" se a variável é do(s) tipo(s) informado(s). 
--- variaveis.py
+++ variaveis.py
@@ -4,12 +4,25 @@ n1 = n2 = n3 = n4 = 0.0
 nome, idade = 'willianDotnet', 40
 status = True
 
-# função 'type'
+# função 'type()'
 print(type(media))
 print(type(n2))
 print(type(n4))
 print(type(nome))
 print(type(idade))
 print(type(status))
-
 # atalho 'ALT + SHIFT + SETA PRA BAIXO' copia a linha atual para a linha de baixo.
+
+# função 'isinstance()'
+altura = 10
+fisico = 'magro'
+print(isinstance(altura, str))
+print(isinstance(altura, float))
+print(isinstance(altura, int))
+print(isinstance(altura, (float, int)))
+# Note que é possível 'perguntar' se a variável é de mais de um tipo usando a função apenas uma vez.
+
+altura = 180
+peso = 90
+resultado = altura * peso
+print(resultado)
